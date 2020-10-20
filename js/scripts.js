00260336e922cd8a5898da44e02269ea0d037f8a
Randel M <Randel.C.Moore@gmail.com>
2020-10-20 16:08:14 -0700

    add code to remove list items on click
    co-authored-by: Parker Benoit <parker.benoit44@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,34 @@
 $(document).ready(function() {
   $("button#hello").click(function() {
-    $(".user").prepend("<li>Hello!</li>");
-    $(".webpage").prepend("<li>Why hello there!</li>");
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#goodbye").click(function() {
-    $(".user").prepend("<li>Goodbye!</li>");
-    $(".webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("#user").prepend("<li>Goodbye!</li>");
+    $("#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#stop").click(function() {
-    $(".user").prepend("<li>Stop copying me!</li>");
-    $(".webpage").prepend("<li>Pardon me. I mean no offense.</li>");
+    $("#user").prepend("<li>Stop copying me!</li>");
+    $("#webpage").prepend("<li>Pardon me. I mean no offense.</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 });
